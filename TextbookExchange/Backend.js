9e6b566793b4a7f0da275ab27998ea76f97e1473
AnPing Tao <anppgtao@gmail.com>
2022-11-10 18:36:15 -0800

    modified addPost and added addMessage functions in Backend class

--- TextbookExchange/Backend.js
+++ TextbookExchange/Backend.js
@@ -13,11 +13,18 @@ import {
 
 import {
     getFirestore,
-    collection, setDoc, doc, getDoc, updateDoc
+    collection, setDoc, doc, getDoc, updateDoc, addDoc
 } from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
+    apiKey: "AIzaSyAPXF-1VTA6-O76lVI3T6Tws894coJ2xrQ",
+    authDomain: "bookschange-d09eb.firebaseapp.com",
+    projectId: "bookschange-d09eb",
+    storageBucket: "bookschange-d09eb.appspot.com",
+    messagingSenderId: "1065896407600",
+    appId: "1:1065896407600:web:acc6efc17988f7bb8cb3f5",
+    measurementId: "G-TWVJVKK33M"
 };
 
 // Initialize Firebase
@@ -101,7 +108,7 @@ class Backend {
     async getUserInfo(username, password) {
         const uid = await this.signIn(username, password);
 
-        const docRef = doc(this.#userDataBase, "users", uid);
+        const docRef = doc(this.#userDataBase, "users", String(uid));
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
@@ -161,12 +168,17 @@ class Backend {
     //                  false on fail
     async addPost(post) {
         try {
-            await setDoc(doc(this.#userDataBase,
-                "posts", String(post.post_id)),
+            const docRef = await addDoc(collection(this.#userDataBase, "posts"),
                 post
             );
 
-            return true;
+            // Set the "post_id" field with the randomly generated
+            // post id
+            await updateDoc(docRef, {
+                "post_id": docRef.id
+            });
+
+            return docRef.id;
         } catch (error) {
             console.log(error);
             return false;
@@ -191,12 +203,35 @@ class Backend {
             return false
         }
     }
+
+    // addMeassage - an async function used to add new message to
+    // the message collection in the databse
+    // parameters: message: Message object
+    // Return value: the message_id of the new added message
+    async addMessage(message) {
+        try {
+            const docRef = await addDoc(collection(this.#userDataBase, "messages"),
+                message
+            );
+
+            // Set the "post_id" field with the randomly generated
+            // post id
+            await updateDoc(docRef, {
+                "message_id": docRef.id
+            });
+
+            return docRef.id;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
 }
 
 // part beyond this point are tests that I have done for the functionality
 // of this module
 
-// Backend = new Backend();
+Backend = new Backend();
 
 /* const uid = await bn.signIn('anpgtao@gmail.com', '987654321');
 
@@ -204,7 +239,9 @@ console.log(uid) */
 
 // const uid = await Backend.signUp("apgtao@gmail.com", "12345678")
 
-// const test = await Backend.getUserInfo("apgtao@gmail.com", "12345678");
+/* const test = await Backend.getUserInfo("apgtao@gmail.com", "12345678");
+
+console.log(test); */
 
 /* console.log(await Backend.addUser({
     uid: "1234567", email: "7654321",
@@ -212,3 +249,5 @@ console.log(uid) */
 })); */
 
 // console.log(await Backend.updatePost({post_id: 1234567, title: "welcome", content: "left"}));
+
+console.log(await Backend.addPost({ user_id: 1234567, post_content: "hello world" }))
