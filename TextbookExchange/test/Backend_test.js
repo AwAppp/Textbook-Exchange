53796f1cb35dafcdfc3d64506b3d5041350c3d1d
AnPing Tao <anppgtao@gmail.com>
2022-12-01 22:02:37 -0800

    add test code for Backend

--- TextbookExchange/test/Backend_test.js
+++ TextbookExchange/test/Backend_test.js
@@ -1,9 +1,55 @@
+/* This module is used for testing the Backend module of this
+project */
+
 import Backend from '../Backend.js';
+
+import { strict as assert } from 'assert';
+import { type } from 'os';
 
 const bk = new Backend();
 
-// console.log(await bk.signIn("apgtao@gmail.com", "12345678"));
+// TODO: to test the code, the first step is to fill these
+//          two variables.
 
-// console.log(await bk.listPosts());
+var test_username = "abcd@gmail.com";
 
-console.log(await bk.getBuyingPosts());
+var test_password = "12345678";
+
+assert(test_username != "", "Please enter the username for test");
+
+assert(test_password != "", "Please enter the password for test");
+
+// sign in with wrong password
+var uid = await bk.signIn(test_username, test_password);
+
+if(typeof(uid) == "object" && uid.hasOwnProperty('code') && uid.code == "auth/user-not-found") {
+    uid = await bk.signUp(test_username, test_password);
+}
+
+uid = await bk.signIn(test_username, test_password + "abcd");
+
+assert(typeof(uid) == "object" && uid.code == "auth/wrong-password");
+
+uid = await bk.signIn(test_username, test_password);
+
+assert(typeof(uid) == "string");
+
+var userInfo = await bk.getUserInfo(test_username, test_password);
+
+assert(userInfo.uid == uid, "uid does NOT match with the one in firestore");
+
+const test_num = Math.random();
+
+await bk.updateUser({test_field: test_num, uid: uid});
+
+assert.equal((await bk.getUserInfoByUid(uid)).test_field, test_num);
+
+var post_id = await bk.addPost({content: "hello world"});
+
+assert(typeof(post_id) == "string");
+
+assert(await bk.updatePost({post_id: post_id, content: "test"}));
+
+const allPost = await bk.listPosts();
+
+assert(typeof(allPost) == "object");
